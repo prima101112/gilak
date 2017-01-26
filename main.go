282712e6e451169b8342653374fd526b78a154c4
prima101112 <prima101112@gmail.com>
2017-01-26 11:18:09 +0700

    initial pong

--- main.go
+++ main.go
@@ -7,9 +7,17 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "aku udah gilak")
-	})
+	http.HandleFunc("/", Root)
+	http.HandleFunc("/ping", Pong)
 
+	log.Println("server running")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func Pong(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Pong")
+}
+
+func Root(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Welcome to gilak</h1>")
 }
